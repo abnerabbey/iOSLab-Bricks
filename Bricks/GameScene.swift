20486f32828969c4ace41c5c7bcf6f3d2a52dda1
Alejandro Mendoza <zippi180@gmail.com>
2019-04-02 13:42:07 -0600

    Added a paddle to  GameScene

--- Bricks/GameScene.swift
+++ Bricks/GameScene.swift
@@ -11,10 +11,19 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var paddle: Paddle?
+    
     override func didMove(to view: SKView) {
         self.backgroundColor = .white
         view.showsFPS = true
         view.showsNodeCount = true
+        
+        let paddleSize = CGSize(width: 150, height: 25)
+        let paddleOriginPosition = CGPoint(x: view.frame.width / 2, y: 100)
+        
+        paddle = Paddle(color: .cyan, size: paddleSize , position: paddleOriginPosition)
+        
+        addChild(paddle!.node)
     }
 
 }
